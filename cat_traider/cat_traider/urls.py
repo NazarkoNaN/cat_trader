bbdd1784055ec8d07479857d0c8d0f5e65c93675
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-11-19 18:32:09 -0800

    Deleting api and starting userpage app

--- cat_traider/cat_traider/urls.py
+++ cat_traider/cat_traider/urls.py
@@ -1,13 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
-from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path('user/', include('cat_user.urls')),
     path('wallet', include('cat_wallet.urls')),
-    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth', views.obtain_auth_token)
 ]
